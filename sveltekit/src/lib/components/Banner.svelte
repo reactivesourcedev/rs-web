a5efcc6f4ebd2b09170590ee8c7ff2ff3bdd1020
reactivesourcedev <reactivesourcedev@gmail.com>
2025-04-07 12:46:40 -0500

    remove temp css boostrap

--- sveltekit/src/lib/components/Banner.svelte
+++ sveltekit/src/lib/components/Banner.svelte
@@ -29,4 +29,53 @@
 </div>
 
 <style>
+    .main .text {
+        line-height: 50px;
+        font-weight: 600;
+        font-size: 64px;
+        margin-bottom: 20px;
+    }
+
+    .main h6.copy { 
+        font-size: 20px;
+        font-weight: 400;
+        line-height: 1.6em;
+        letter-spacing: .016em;
+    }
+
+    .main {
+        height: 90vh;
+        text-align: center;
+        display: flex;
+        align-items: center;
+    }
+
+    .main-text {
+    display: table-cell;
+    vertical-align: middle;
+    }
+
+    .main-logo {
+        width: 280px;
+        margin-bottom: 50px;
+    }
+
+    .text .content { display: inline; }
+
+    .text .dash {
+        display: inline-block;
+        height: 8px;
+        width: 30px;
+        background-color: #fff;
+        animation: blink 1.5s infinite linear;
+    }
+
+    .copy .dash {
+        display: inline-block;
+        height: 2px;
+        width: 12px;
+        background-color: #fff;
+        animation: blink 1.5s infinite linear;
+    }
+    
 </style>
